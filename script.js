eaeb42e796e3e8265f8639845276c856295f902f
Vulla Sai kiran kumar <110409176+SaikiranVulla@users.noreply.github.com>
2022-08-08 17:09:14 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,26 +1,29 @@
-document.getElementById("totaltip").style.display = "none";
-document.getElementById("container").onclick = function() { calculatetip();};
+
 
 function calculatetip()
 {
   var billamount= document.getElementById("billamount").value;
   var servicequality = document.getElementById("servicequality").value;
-  var numPeople = document.getElementById("totalmember").value;
-  if(billamount == "" || servicequality == "") {
+  var numPeople = document.getElementById("totalpeople").value;
+  if(billamount === "" || servicequality == "") {
     window.alert("Please enter any value");
     return;
 }  
 
-if(numPeople == "" || numPeople <= 1){
+if(numPeople === "" || numPeople <= 1){
   numPeople = 1;
-  document.getElementById("totaltip").style.display = "none";
   document.getElementById("each").style.display = "block";
 }
 else{
   document.getElementById("each").style.display = "block";
 }
-var total = (billamount * servicequality) / totalmember;
+var total = (billamount * servicequality) / numPeople;
 document.getElementById("totaltip").style.display = "block";
 document.getElementById("tip").innerHTML = total;
 
 }
+
+
+document.getElementById("totaltip").style.display = "none";
+document.getElementById("each").style.display = "none";
+document.getElementById("calculate").onclick = function() { calculatetip();};
